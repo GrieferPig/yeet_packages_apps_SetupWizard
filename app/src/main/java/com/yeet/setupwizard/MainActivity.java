57fa3d91b296bc1cd14c66378801b66c13fe0eac
GrieferPig <grieferpig@163.com>
2022-10-16 12:12:39 +0800

    feat: Added Pager

--- app/src/main/java/com/yeet/setupwizard/MainActivity.java
+++ app/src/main/java/com/yeet/setupwizard/MainActivity.java
@@ -3,12 +3,53 @@ package com.yeet.setupwizard;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
+
+    ViewFlipper vf;
+    LinearLayout btn_navGroup;
+    int pageCounter = 0;
+
+    final int totalPageNum = 3 - 1; // started from zero
+
+    final String TAG = "TAG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        vf = findViewById(R.id.vf);
+        btn_navGroup = findViewById(R.id.btn_navGroup);
+    }
+
+    public void onWelcomeNextClick(View v){
+        btn_navGroup.setVisibility(View.VISIBLE);
+        gotoNextPage(v);
+    }
+
+    public void gotoNextPage(View v){
+        pageCounter++;
+        vf.setDisplayedChild(pageCounter);
+        if(pageCounter == totalPageNum){
+            btn_navGroup.setVisibility(View.GONE);
+        }
+        Log.d(TAG, "gotoNextPage: "+pageCounter);
+    }
+
+    public void gotoPrevPage(View v){
+        pageCounter--;
+        vf.setDisplayedChild(pageCounter);
+        if(pageCounter == 0){
+            btn_navGroup.setVisibility(View.GONE);
+        }
+        Log.d(TAG, "gotoNextPage: "+pageCounter);
+    }
+
+    public void finish(View v){
+        finish();
     }
 }
